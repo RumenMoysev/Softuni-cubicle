7375bed1f4426626eb6bd82a747e4e8066ff1284
RumenMoysev <rumoysev@gmail.com>
2023-09-20 23:18:25 +0300

    Add cubesController

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const expressConfigurator = require('./config/expressConfigurator.js')
 const handlebarsConfigurator = require('./config/handlebarsConfigurator.js')
 
 const homeController = require('./controllers/homeController.js')
+const cubesController = require('./controllers/cubesController.js')
 
 expressConfigurator(app)
 handlebarsConfigurator(app)
 
 app.use(homeController)
-
+app.use('/cubes', cubesController)
 
 app.listen(SERVER_PORT, () => {console.log(`Server listening on port ${SERVER_PORT}.`)})
---
+++ src/managers/cubeManager.js
@@ -0,0 +1,32 @@
+const uniqId = require('uniqid')
+
+const cubes = [
+    {
+        imageUrl: 'https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg',
+        name: 'Gan356 Air SM',
+        difficultyLevel: 3,
+        description: 'a Rubic cube',
+        id: uniqId()
+    },
+    {
+        imageUrl: 'https://www.awesomeinventions.com/wp-content/uploads/2016/10/Simple-Rubiks-Cube.jpg',
+        name: 'Dragancho',
+        difficultyLevel: 1,
+        description: 'a Rubics nigga cube',
+        id: uniqId()
+    },
+    {
+        imageUrl: 'https://images.interestingengineering.com/images/JULY/Erno_Rubik_Magic_Cube.jpg',
+        name: 'Rumen',
+        difficultyLevel: 6,
+        description: 'the hardest one',
+        id: uniqId()
+    }
+]
+
+exports.getCubes = () => cubes.slice()
+
+exports.addCube = (cube) => {
+    cube.id = uniqId()
+    cubes.push(cube)
+}
