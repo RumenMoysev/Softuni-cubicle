487faaa228ab9bcb709a72372f5224c23144ea92
RumenMoysev <rumoysev@gmail.com>
2023-09-20 22:35:29 +0300

    Add express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('woks')
+const express = require('express');
+
+const app = express()
+const SERVER_PORT = 5000
+
+
+app.listen(SERVER_PORT, () => {console.log(`Server listening on port ${SERVER_PORT}.`)})
