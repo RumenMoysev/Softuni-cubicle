fa2bd817d6066b53bfec24cbf78253d964c0aed4
RumenMoysev <rumoysev@gmail.com>
2023-09-21 19:21:43 +0300

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,12 @@ const cubesController = require('./controllers/cubesController.js')
 router.use(homeController)
 router.use('/cubes', cubesController)
 
+router.get('/404', (req, res) => {
+    res.render('404')
+})
+
+router.get('*', (req, res) => {
+    res.redirect('/404');
+})
+
 module.exports = router 
