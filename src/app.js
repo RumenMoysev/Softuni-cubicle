14e4d44213569c290a12758e4e8acc40516685e0
RumenMoysev <rumoysev@gmail.com>
2023-09-21 19:56:04 +0300

    Moved routes to a router file

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
-const SERVER_PORT = 5000
-const app = express()
+const SERVER_PORT = 5000;
+const app = express();
 
-const expressConfigurator = require('./config/expressConfigurator.js')
-const handlebarsConfigurator = require('./config/handlebarsConfigurator.js')
+const expressConfigurator = require('./config/expressConfigurator.js');
+const handlebarsConfigurator = require('./config/handlebarsConfigurator.js');
 
-const router = require('./routes.js')
+const router = require('./routes.js');
 
-expressConfigurator(app)
-handlebarsConfigurator(app)
+expressConfigurator(app);
+handlebarsConfigurator(app);
 
-app.use(router)
+app.use(router);
 
-app.listen(SERVER_PORT, () => {console.log(`Server listening on port ${SERVER_PORT}.`)})
+app.listen(SERVER_PORT, () => {console.log(`Server listening on port ${SERVER_PORT}.`)});
