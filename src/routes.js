0d92d71e543e6d66818a1271b18837c0cbf306c5
RumenMoysev <rumoysev@gmail.com>
2023-09-21 19:40:18 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
-const router = require('express').Router()
-const homeController = require('./controllers/homeController.js')
-const cubesController = require('./controllers/cubesController.js')
+const router = require("express").Router();
+const homeController = require("./controllers/homeController.js");
+const cubesController = require("./controllers/cubesController.js");
 
+router.use(homeController);
+router.use("/cubes", cubesController);
 
-router.use(homeController)
-router.use('/cubes', cubesController)
+router.get("/404", (req, res) => {
+    res.render("404");
+});
 
-router.get('/404', (req, res) => res.render('404'))
+router.get("*", (req, res) => {
+    res.redirect("/404");
+});
 
-router.get('*', (req, res) => res.redirect('/404'))
-
-module.exports = router 
+module.exports = router;
