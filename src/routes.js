87a19d69451eab17b23804b198e6e79492c5cdb6
RumenMoysev <rumoysev@gmail.com>
2023-09-21 19:34:26 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,8 @@ const cubesController = require('./controllers/cubesController.js')
 router.use(homeController)
 router.use('/cubes', cubesController)
 
-router.get('/404', (req, res) => {
-    res.render('404')
-})
+router.get('/404', (req, res) => res.render('404'))
 
-router.get('*', (req, res) => {
-    res.redirect('/404');
-})
+router.get('*', (req, res) => res.redirect('/404'))
 
 module.exports = router 
