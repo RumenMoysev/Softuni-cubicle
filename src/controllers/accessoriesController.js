3838180eed846e3f1aa6072bdfad233ac56a3c67
RumenMoysev <rumoysev@gmail.com>
2023-09-28 20:04:04 +0300

    Add create accessory

--- src/controllers/accessoriesController.js
+++ src/controllers/accessoriesController.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 
+const accessoryManager = require('../managers/accessoryManager.js')
+
 router.get('/add-accessory', (req, res) => {
     res.render('accessoryTemps/create')
+})
+
+router.post('/add-accessory', async (req, res) => {
+    const body = req.body
+
+    const cubeData = {
+        imageUrl: body.imageUrl,
+        name: body.name,
+        description: body.description
+    }
+
+    await accessoryManager.create(cubeData)
+    res.redirect('/')
 })
 
 module.exports = router
