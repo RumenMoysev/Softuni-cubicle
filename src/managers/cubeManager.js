fed5098a1795ce5c3af9d7f4a6e3cecd02684d15
RumenMoysev <rumoysev@gmail.com>
2023-09-28 09:52:23 +0300

    Removed cubes array

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,30 +1,6 @@
 const uniqId = require('uniqid')
 const Cube = require('../models/Cube.js')
 
-const cubes = [
-    {
-        imageUrl: 'https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg',
-        name: 'Gan356 Air SM',
-        difficultyLevel: 3,
-        description: 'a Rubic cube',
-        id: uniqId()
-    },
-    {
-        imageUrl: 'https://www.awesomeinventions.com/wp-content/uploads/2016/10/Simple-Rubiks-Cube.jpg',
-        name: 'Dragancho',
-        difficultyLevel: 1,
-        description: 'a Rubics nigga cube',
-        id: uniqId()
-    },
-    {
-        imageUrl: 'https://images.interestingengineering.com/images/JULY/Erno_Rubik_Magic_Cube.jpg',
-        name: 'Rumen',
-        difficultyLevel: 6,
-        description: 'the hardest one',
-        id: uniqId()
-    }
-]
-
 exports.getCubes = () => Cube.find()
 exports.getCubesLean = () => Cube.find().lean()
 
