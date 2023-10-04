b9b9bfe7df94024347421fbae06e027b7ac1e163
RumenMoysev <rumoysev@gmail.com>
2023-10-04 11:21:22 +0300

    show register page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController.js");
 const cubesController = require("./controllers/cubesController.js");
 const accessoriesController = require('./controllers/accessoriesController.js');
+const userController = require('./controllers/userController.js')
 
 router.use(homeController);
 router.use("/cubes", cubesController);
 router.use("/accessories", accessoriesController);
+router.use("/users", userController)
 
 router.get("/404", (req, res) => {
     res.render("404");
