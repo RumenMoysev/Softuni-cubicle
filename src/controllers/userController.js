a32204d872fc94532393ea1592dc35a5345de9d0
RumenMoysev <rumoysev@gmail.com>
2023-10-04 11:22:23 +0300

    show login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 
 router.get('/register', (req, res) => {
     res.render('userTemps/register')
+});
+
+router.get('/login', (req, res) => {
+    res.render('userTemps/login')
 })
 
 module.exports = router
